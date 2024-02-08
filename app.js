47a6deb05606017f10d85e181db83caec1931bd4
tanaji <tanajipatil9696@gmail.com>
2024-02-08 16:31:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT || 3000; // Port number can be customized
+const cors = require('cors');
+
+const userRouter = require('./routes/users.routes');
+const productRouter = require('./routes/product.routes');
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+// Define routes here
+app.use(cors())
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Connect to MongoDB Atlas
+ const uri = "mongodb+srv://tanajipatil9696:8wcaSvi7ho8dQlSJ@tanaji123.cszlwpu.mongodb.net/?retryWrites=true&w=majority";//process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB Atlas'))
+    .catch(err => console.error('Error connecting to MongoDB Atlas', err));
+
+
+app.use('/users', userRouter);
+app.use('/products', productRouter);
---
+++ routes/product.routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/product.model');
+
+// Get all users
+router.get('/getproducts', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Create a new user
+router.post('/addproduct', async (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description
+    });
+
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+module.exports = router;
